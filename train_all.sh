80bb6b1fb9690e77191f9145c135d62114f95412
Mostafa Ajallooeian <mostafa.ajallooeian@unit8.co>
2021-07-07 14:05:33 +0200

    Remove train script

--- train_all.sh
+++
@@ -1,4 +0,0 @@
-# python -m src.train_agree_add_loop --init_weights 1
-python -m src.train_agree_add_loop
-python -m src.train_agree_add_loop --base_model_name models/nlu_evaluation_data/mlm/checkpoint-50420
-python -m src.train_agree_add_loop --use_fasttext 1
